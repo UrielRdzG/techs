abb989e180d3f009f19d6fba2ae322dce8064378
UrielRdzG <61171842+UrielRdzG@users.noreply.github.com>
2020-02-19 19:19:06 -0600

    algo2

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pump It Up</title>
+	<link rel="stylesheet" type="text/css" href="css/master.css">
+</head>
+<body background="img/fondo1.jpg" >
+	<section >
+	<header id="main-header">
+		
+		<a id="logo-header" href="index.html">
+			<span class="site-name">Pump it Up</span>
+			<span class="site-desc">PIU</span>
+		</a> <!-- / #logo-header -->
+ 
+		<nav>
+			<ul>
+				<li><a href="index.html">Inicio</a></li>
+				<li><a href="gamemodes.html">Modalidades de juego</a></li>
+				<li><a href="softwareversions.html">Versiones de software</a></li>
+			</ul>
+		</nav><!-- / nav -->
+ 
+	</header><!-- / #main-header -->
+	</section>
+
+	<section>
+		<div id="principal">
+			<br><br><br><br>
+			<div align="center"><video src="multimedia/Opening2.mp4" width="800" height="500" controls autoplay></video></div>
+			<p id="text-1">
+				Pump It Up abreviado también como PIU o simplemente acortado a Pump, es una serie de videojuegos arcade coreana de simulación de baile lanzado por Andamiro en 1999. Este juego sigue en competencia con Dance Dance Revolution, serie de videojuegos japonesa. La interfáz originalmente estaba en coreano, pero en Las Américas, Europa y parte de Asia, se hizo traducciones al inglés o al idioma de la localización. Con Pump it Up Prime, sería la primera entrega coreana lanzada en Japón, remplazando la interfaz coreana por la japonesa y de conexión se usa Sega All.net y Aime de Bandai Namco Entertainment en algunas localidades. Consta de una máquina con una pantalla, altavoces, luces y un tablero de diez paneles que se coloca en el suelo, este a su vez consta de botones con ocho flechas y dos botones centro, en la pantalla se indica cual o cuales se debe pisar para lograr una puntuación; un jugador individual solo podría jugar con cinco paneles, según se desee el modo de jugabilidad.
+
+
+			</p>
+			<br>
+			<p id="text-1">
+				En la pantalla aparece un destellante neón con todas las flechas y además, flechas de colores que señalan qué sensores son los que se deben pisar y el orden para pisarlos, incluyendo un video que sirve tanto como apoyo visual de la canción, como un medio de entretenimiento al jugador.
+			</p>
+			<br><br>
+			<div  align="center"><img src="img/piu_xx_logoM.png"  id="logoM"></div>
+			<br><br><br><br><br><br><br></div>
+	</section>
+	<section>
+		<footer id="footer-pump">
+		 <!-- / #logo-header -->
+ 		<div align="center"><img src="img/andamiro-logo.png"  id="logoA"><br><p>Copyright ⓒ Andamiro Co., LTD. All rights reserved.</p></div>
+		<div id="footer-social" align="center">
+
+			<a href=""><img src="img/face.png" width="60" height="60"></a>
+			
+			<a href=""><img src="img/twitter.png" width="60" height="60"></a>
+			
+			<a href=""><img src="img/youtube.png" width="60" height="60"></a>
+		</div>
+		</footer>
+	</section>
+</body>
+</html>
